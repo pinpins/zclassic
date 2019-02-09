f363bcc8c7daf266539b6c7110c71c6d7da74da7
skellers <iain21lawrie@gmail.com>
2019-02-09 18:01:08 +0000

    mainnet minimum chain work

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -108,7 +108,7 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_SAPLING].nActivationHeight = 500000;
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000000000281b32ff3198a1");
+        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000721bfb84aedeb");
 
         /**
          * The message start string should be awesome! ⓩ❤
